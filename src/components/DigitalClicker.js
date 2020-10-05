1655292aefb0ca0c192db3e8a6852c5f10b9ca6e
aaronfeingold <ajfeingold88@gmail.com>
2020-10-05 16:12:19 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,33 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export class DigitalClicker extends Component {
+
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  handleClick = () => {
+    // Update our state here...
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    }, () => console.log(this.state))
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export class YouTubeDebugger extends Component {
+  
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleResolutionChange = () => {
+    // Update our state here...
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    })
+  };
+  handleBitrateChange = () => {
+    // Update our state here...
+    this.setState({
+      settings:{
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  };
+
+  render() {
+    return (
+      <div>
+         <button className="bitrate" onClick={this.handleBitrateChange}></button>
+         <button className="resolution" onClick={this.handleResolutionChange}></button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger
